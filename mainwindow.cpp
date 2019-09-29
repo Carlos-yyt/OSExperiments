9a21f42e9423fe659f998ef34654ae75220f7e44
Carlos-yyt <302642469@qq.com>
2019-09-29 12:11:43 +0800

    Signed-off-by: Carlos-yyt <302642469@qq.com>

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,28 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    DWORD size=GetModuleFileNameA(nullptr,path,MAX_PATH);//MAX_PATH在minwindef.h的第33行有定义：#define MAX_PATH 260
+    pathStr=path;
+    if (size != 0)
+        qDebug()<<"程序当前目录："+pathStr;//输出当前目录信息
+    else
+        exit(0); //若获取失败，则直接退出程序
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_delExistBtn_clicked(bool checked)
+{
+    WCHAR pathWChar[256];
+    memset(pathWChar,0,sizeof(pathWChar));
+
+    //MultiByteToWideChar函数是将多字节转换为宽字节的一个API函数 char* 转 LPCWSTR
+    MultiByteToWideChar(CP_ACP,0,path,strlen(path)+1,pathWChar,sizeof(pathWChar)/sizeof(pathWChar[0]));
+
+    int fileStatusValue=DeleteFile(pathWChar);
+    //下面继续写文件，在那之前先获取TXT地址
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "windows.h"
+#include <qdebug.h>
 
 namespace Ui {
 class MainWindow;
@@ -15,6 +17,15 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_clicked(bool checked);
+
+    void on_isExistBtn_clicked(bool checked);
+
+    void on_delExistBtn_clicked(bool checked);
+    char* path;//当前文件地址
+    QString pathStr;//当前文件地址的String
+    char* pathTXT;//TXT文件的位置
 private:
     Ui::MainWindow *ui;
 };
