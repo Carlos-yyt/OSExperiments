675bda0f51b471a5920c53766313c4c730592982
Carlos-yyt <302642469@qq.com>
2019-10-02 00:24:25 +0800

    + 还差时钟，明天搞。 + 另外，删除文件也有问题，删不掉。 ============== 10月13日就要考托福了，赶紧写完了专心复习……感觉这一次又要白给。
    Signed-off-by: Carlos-yyt <302642469@qq.com>

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -19,6 +19,7 @@ MainWindow::MainWindow(QWidget *parent) :
         strcat(pathTXT,TXT_FILE_NAME);//加上文件名
         pathTXTStr=pathTXT;
         qDebug()<<"文本文件地址："+pathTXTStr;//输出当前目录信息
+        //charToWChar(pathTXT,pathTXTWChar);//转好字符备用
     }else{
         qDebug()<<"获取文件地址失败";
         exit(0); //若获取失败，则直接退出程序
@@ -30,6 +31,7 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+//字符转换
 bool MainWindow::charToWChar(char* src,WCHAR *dest){
     memset(dest,0,sizeof(dest));
 
@@ -41,6 +43,54 @@ bool MainWindow::charToWChar(char* src,WCHAR *dest){
 //删除现有文件
 void MainWindow::on_delExistBtn_clicked(bool checked)
 {
-    charToWChar(pathTXT,pathTXTWChar);
-    int fileStatusValue=DeleteFile(pathTXTWChar);
+    int fileStatusValue;
+    fileStatusValue=DeleteFile(pathTXTWChar);//TODO 为什么没有文件也能删除成功？找找
+    //qDebug()<<"fileStatusValue："+QString::number(fileStatusValue);
+    //qDebug()<<"ERROR_FILE_NOT_FOUND:"+QString::number(ERROR_FILE_NOT_FOUND);
+    if(fileStatusValue==ERROR_FILE_NOT_FOUND){
+        ui->delFileLab->setText("没有找到文件。可以直接进行程序。");
+    }else if(fileStatusValue==ERROR_ACCESS_DENIED){
+        ui->delFileLab->setText("有文件，但是被设置成了只读文件。");
+    }else {
+        ui->delFileLab->setText("已经找到文件并删除。现在没有文件了。");
+    }
+}
+
+//创建TXT文件
+void MainWindow::on_createFileBtn_clicked(bool checked)
+{
+    pFile=CreateFile(
+                pathTXTStr.toStdWString().c_str(),//文件地址
+                GENERIC_READ|GENERIC_WRITE,//文件可读可写
+                FILE_SHARE_READ,//保护期间，可读不可写
+                NULL,//设为 NULL，子进程不能继承本句柄
+                CREATE_ALWAYS,//创建文件，会改写前一个文件。虽然之前delete过，但出于鲁棒性还是这么写吧。
+                FILE_ATTRIBUTE_NORMAL,//文件属性设置为默认
+                NULL//不设置文件句柄
+                );
+    if (pFile == INVALID_HANDLE_VALUE)
+    {
+        ui->createFileLab->setText("失败！文件未创建。");
+        CloseHandle(pFile);
+    }else {
+        ui->createFileLab->setText("成功！文件已创建。");
+        char FileHead[] = "id\t\tname\t\tinputTime\t\tns\t\truntimes\r\n";//写文件头
+        WriteFile(pFile, FileHead, strlen(FileHead), NULL, NULL);
+    }
+}
+
+void MainWindow::on_writeTXTBtn_clicked(bool checked)
+{
+    char write_content[100] = { 0 }; //写入内容的数组
+
+    for(int id=0;id<10;id++){
+        memset(write_content,0,100);
+        strcat(write_content,QString::number(id).toStdString().data() ); //写入id数
+        strcat(write_content, "\t\t"); //写入间隔
+        strcat(write_content, "像素\t\t\r\n"); //写入名字
+
+        //DWORD SizeToWrite = sizeof(write_content);
+        WriteFile(pFile, write_content, strlen(write_content), NULL, NULL);
+    }
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -4,6 +4,7 @@
 #include <QMainWindow>
 #include "windows.h"
 #include <qdebug.h>
+#include <QString>
 #define TXT_FILE_NAME "19317119_win32.txt"
 
 namespace Ui {
@@ -23,6 +24,10 @@ private slots:
     void on_delExistBtn_clicked(bool checked);
 
     bool charToWChar(char* src,WCHAR *dest);//char字符串向WChar转换
+    void on_createFileBtn_clicked(bool checked);
+
+    void on_writeTXTBtn_clicked(bool checked);
+
 private:
     char path[MAX_PATH];//当前文件地址
     QString pathStr;//当前文件地址--QString
@@ -31,6 +36,7 @@ private:
     QString pathTXTStr;//当前文本文件地址--QString
     WCHAR pathTXTWChar[MAX_PATH];
 
+    HANDLE pFile;//TXT文件指针
 
 private:
     Ui::MainWindow *ui;
