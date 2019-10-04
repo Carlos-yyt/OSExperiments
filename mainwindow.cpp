2c6bec5c85757f8ada77096976ee676c4cb813f4
Carlos-yyt <302642469@qq.com>
2019-10-04 08:30:55 +0800

    报告也改差不多了…… 睡一觉，醒来再看看细节，然后就交了。
    Signed-off-by: Carlos-yyt <302642469@qq.com>

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -49,13 +49,8 @@ bool MainWindow::charToWChar(char* src,WCHAR *dest){
 //删除现有文件
 void MainWindow::on_delExistBtn_clicked(bool checked)
 {
-
     QFile::remove(pathTXTStr);
-    exTimes=0;
-    exDelay=0;
 
-    ui->delayLab->setText(QString::number(exDelay*1.0/(exTimes*10),'g',6));
-    ui->exTimesLcdNum->display(exTimes);//改变记分板上的试验次数
 //    //===DeleteFile===总有问题
 //    int fileStatusValue;
 
@@ -69,7 +64,10 @@ void MainWindow::on_delExistBtn_clicked(bool checked)
 //    }else {
 //        ui->delFileLab->setText("已经找到文件并删除。现在没有文件了。");
 //    }
-
+    exTimes=0;
+    exDelay=0;
+    ui->delayLab->setText(QString::number(exDelay*1.0/(exTimes*10),'g',6));
+    ui->exTimesLcdNum->display(exTimes);//改变记分板上的试验次数
 }
 
 //创建TXT文件
@@ -91,18 +89,25 @@ void MainWindow::on_createFileBtn_clicked(bool checked)
     }else {
         ui->createFileLab->setText("成功！文件已创建。");
         char FileHead[] = "id\t\tname\t\tinputTime\t\tns\t\truntimes\r\n";//写文件头
-        WriteFile(pFile, FileHead, strlen(FileHead), NULL, NULL);
+        WriteFile(pFile, FileHead, strlen(FileHead), NULL, NULL);//写入TXT文件
     }
 }
 
-//获取时间戳
+//获取rdtsc
 inline cycles_time MainWindow::currentcycles(){
     cycles_time result;
         __asm__ __volatile__ ("rdtsc" : "=A" (result));//可惜不能直接返回它的返回值，要走一个中间变量。
         return result;
 }
 
+//连续获取10次rdtsc
 inline void MainWindow::get10NS(){
+    Sleep(0);//释放当前线程所剩余的时间片（如果有剩余的话），尽量让下面20条语句享有完整的时间片
+
+    /*
+     * 1 - 不设置一个不断自增的变量是为了节省运行时的时间。
+     * 2 - 每句写2遍是因为取一次需要10ns，而题目刚好要求20ns。
+     */
     __asm__ __volatile__ ("rdtsc" : "=A" (ns[0]));
     __asm__ __volatile__ ("rdtsc" : "=A" (ns[0]));
 
@@ -148,18 +153,18 @@ void MainWindow::on_writeTXTBtn_clicked(bool checked)
     QTime current_time;//用于获得时分秒
     int rand_num[10];//随机数
 
-    //为了减少写记录for循环的任务量，把rand()的时间放在外面
+    //===生成随机数
     for (int i=0;i<10;i++) {
         rand_num[i]=rand()%50+1;
     }
 
-    //获取主频
+    //===获取主频
     mFre=getThisMachineFrequency();
 
-    //10个ns时间
+    //===获取10个rdtsc
     get10NS();
 
-    //方便多次不退出程序多次实验，加个实验编号
+    //===实验组号，方便多次实验时观察记录
     strcat(write_content,"第");
     strcat(write_content,QString::number(++exTimes).toStdString().data());
     strcat(write_content,"次实验（插入10条记录）");
@@ -168,7 +173,7 @@ void MainWindow::on_writeTXTBtn_clicked(bool checked)
 
     //===写记录，循环写入10条
     for(int id=0;id<10;id++){
-        memset(write_content,0,100);//清空
+        memset(write_content,0,100);//清空write_content数组
 
         //===接上id===
         strcat(write_content,QString::number(id).toStdString().data());//写入记录的编号
@@ -183,31 +188,34 @@ void MainWindow::on_writeTXTBtn_clicked(bool checked)
 
         //===接上ns===
         strcat(write_content, "\t\t"); //接上间隔
-        strcat(write_content,QString::number((DWORD)(((double(ns[id]))/mFre)*1000000)).toStdString().data()); //接上ns，不设置很多中间变量是为了减少开支
-
+        strcat(write_content,QString::number((DWORD)(((double(ns[id]))/mFre)*1000000)).toStdString().data()); //接上ns
         //===接上随机数===
         strcat(write_content, "\t"); //接上间隔
         strcat(write_content, QString::number(rand_num[id]).toStdString().data()); //写入随机数
         strcat(write_content, "\r\n"); //换行
-        WriteFile(pFile, write_content, strlen(write_content), NULL, NULL);
+
+        //===写文件===
+        WriteFile(pFile, write_content, strlen(write_content), NULL, NULL);//向TXT文件写入
     }
 
+    //将本组间隔时间加入累计
     for(int i=0;i<9;i++){
         exDelay+=(int)(((double(ns[i+1]-ns[i]))/mFre)*1000000);
     }
 
-    ui->delayLab->setText(QString::number(exDelay*1.0/(exTimes*9),'g',6));//不是exTimes*10！我真是蠢死了。
-    ui->exTimesLcdNum->display(exTimes);//改变记分板上的试验次数
+    ui->delayLab->setText(QString::number(exDelay*1.0/(exTimes*9),'g',6));//更新平均间隔时间。不是exTimes*10！我真是蠢死了。
+    ui->exTimesLcdNum->display(exTimes);//更新记分板上的试验次数。
 }
 
 void MainWindow::on_doManyExBtn_clicked()
 {
-    int exTimes=0;
+    int exTimes=0;//接收用户指定的实验次数
     exTimes=ui->doManyExLineEdit->text().toInt();
     for(int i=0;i<exTimes;i++){
-        on_writeTXTBtn_clicked(true);
+        on_writeTXTBtn_clicked(true);//一次实验
     }
 }
+
 void MainWindow::on_doManyExBtn_clicked(bool checked)
 {
 
--- mainwindow.h
+++ mainwindow.h
@@ -26,41 +26,42 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);//生成EXE文件和TXT文件的地址的各种编码
     ~MainWindow();
 
 private slots:
 
-    void on_delExistBtn_clicked(bool checked);
+    void on_delExistBtn_clicked(bool checked);//检查并删除现有文件
 
     bool charToWChar(char* src,WCHAR *dest);//char字符串向WChar转换
-    void on_createFileBtn_clicked(bool checked);
 
-    void on_writeTXTBtn_clicked(bool checked);
+    void on_createFileBtn_clicked(bool checked);//创建一个TXT文件并写入文件表头
 
-    void on_doManyExBtn_clicked();
+    void on_writeTXTBtn_clicked(bool checked);//向TXT文件写入一组（10条）记录
+
+    void on_doManyExBtn_clicked();//进行多次实验
 
     void on_doManyExBtn_clicked(bool checked);
 
 private:
-    char path[MAX_PATH];//当前文件地址
-    QString pathStr;//当前文件地址--QString
+    char path[MAX_PATH];//当前程序地址
+    QString pathStr;//当前程序地址--QString形式
 
     char pathTXT[MAX_PATH];//TXT文件的位置
-    QString pathTXTStr;//当前文本文件地址--QString
-    WCHAR pathTXTWChar[MAX_PATH];
+    QString pathTXTStr;//当前TXT文件地址--QString形式
+    WCHAR pathTXTWChar[MAX_PATH];//当前TXT文件地址--WCHAR形式
 
     HANDLE pFile;//TXT文件指针
 
     machineFrequency mFre;//本机主频
-    cycles_time ns[10];
+    cycles_time ns[10];//存放10次获取rdtsc的数组
 
-    inline cycles_time currentcycles();//获取一次当前的时间戳
-    inline void get10NS();//连续获取10个NS时间
+    inline cycles_time currentcycles();//获取当前的rdtsc
+    inline void get10NS();//连续获取10个rdtsc
     inline machineFrequency getThisMachineFrequency();//获取本机主频
 
-    int exTimes=0;//试验次数
-    int exDelay=0;//总延迟
+    int exTimes=0;//已经进行的实验次数
+    int exDelay=0;//所有实验产生的间隔（每次实验产生9次间隔）
 
 private:
     Ui::MainWindow *ui;
